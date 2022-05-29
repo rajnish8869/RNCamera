d004380990dc2bdf2c057a4687bd697a1ea27def
Stuti Singhal <stutisinghal1312@gmail.com>
2022-05-29 11:04:08 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import Camera from './src/Camera';
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+      <Camera />
+    </View>
+  );
+};
+
+export default App;
---
+++ src/Camera.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { RNCamera, FaceDetector } from 'react-native-camera';
+
+const Camera = () => {
+  return (
+    <View>
+      <Text>Camera</Text>
+    </View>
+  )
+}
+
+export default Camera
+
+const styles = StyleSheet.create({})
